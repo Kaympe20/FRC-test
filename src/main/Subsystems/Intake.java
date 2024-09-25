b874bcc9015bb71095edb47eda708e143def6b04
Kayla <kayla.blosser@proton.me>
2024-09-25 10:17:30 -0400

    Declare all parts of the intake

--- src/main/Subsystems/Intake.java
+++ src/main/Subsystems/Intake.java
@@ -4,14 +4,27 @@
 
 package Intake;
 
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.AnalogEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
   /** Creates a new Intake. */
-  public Intake() {
-    
-  }
+  private CANSparkMax intakeWrist = new CANSparkMax(1, MotorType.kBrushless);
+  private CANSparkMax intakeRoller = new CANSparkMax(2, MotorType.kBrushless);
+  private DigitalInput beamBreak = new DigitalInput(0);
+  private AnalogEncoder wristEncoder = new AnalogEncoder(0);
+  private PIDController pid = new PIDController(kP, kI, kD);
 
+
+  public Intake() {
+
+  }
+  public void intakeIn(){
+    //intake motor in
+
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
