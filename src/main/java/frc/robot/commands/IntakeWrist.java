22229615f23fc552d7b25010eeb1a8e043b06548
Kayla <kayla.blosser@proton.me>
2024-09-27 12:57:51 -0400

    Move code to correct directory

--- src/main/java/frc/robot/commands/IntakeWrist.java
+++ src/main/java/frc/robot/commands/IntakeWrist.java
@@ -2,8 +2,10 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package IntakeWrist;
+package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.AnalogEncoder;
+import com.revrobotics.CANSparkMax;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
 
@@ -12,12 +14,16 @@ import edu.wpi.first.wpilibj2.command.PIDCommand;
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class IntakeWrist extends PIDCommand {
   /** Creates a new IntakeWrist. */
+  
+private AnalogEncoder wristEncoder = new AnalogEncoder(0);
+private CANSparkMax intakeWrist = new CANSparkMax(1, MotorType.kBrushless);
+
   public IntakeWrist() {
     super(
         // The controller that the command will use
         new PIDController(0, 0, 0),
         // This should return the measurement
-        () -> 0,
+        () -> wristEncoder.get,
         // This should return the setpoint (can also be a constant)
         () -> 0,
         // This uses the output
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package Intake;
+package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 
@@ -11,19 +11,16 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
   /** Creates a new Intake. */
-  private CANSparkMax intakeWrist = new CANSparkMax(1, MotorType.kBrushless);
   private CANSparkMax intakeRoller = new CANSparkMax(2, MotorType.kBrushless);
   private DigitalInput beamBreak = new DigitalInput(0);
-  private AnalogEncoder wristEncoder = new AnalogEncoder(0);
-  private PIDController wristPID = new PIDController(kP, kI, kD);
+  private PIDController wristPID = new PIDController(1, 1, 0);
 
 
   public Intake() {
-
+    wristPID.enable();
   }
-  public void intakeIn(){
-    //intake motor in
-
+  public void extendWrist(){
+    
   }
   @Override
   public void periodic() {
