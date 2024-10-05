19ed31f9eadfa715822a1a4b9dd57b681f05747a
Kayla <kayla.blosser@proton.me>
2024-10-05 13:05:53 -0400

    Update Wrist PID Command

--- src/main/java/frc/robot/commands/IntakeWrist.java
+++ src/main/java/frc/robot/commands/IntakeWrist.java
@@ -8,26 +8,28 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
 
 //importing device libraries
-import edu.wpi.first.wpilibj.AnalogEncoder;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
+// import com.revrobotics.CANSparkLowLevel.MotorType;
+import java.util.function.DoubleSupplier;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class IntakeWrist extends PIDCommand {
   /** Creates a new IntakeWrist. */
-  public IntakeWrist() {
+
+  public IntakeWrist(DoubleSupplier sensorVal, DoubleSupplier setpoint,  CANSparkMax wrist) {
     super(
         // The controller that the command will use
-        new PIDController(0, 0, 0),
+        new PIDController(1, 1, 1),
         // This should return the measurement
-        () -> 0,
+        sensorVal,
         // This should return the setpoint (can also be a constant)
-        () -> 0,
+        5, //placeholder value
         // This uses the output
         output -> {
           // Use the output here
+          wrist.set(output);
         });
     // Use addRequirements() here to declare subsystem dependencies.
     // Configure additional PID options by calling `getController` here.
