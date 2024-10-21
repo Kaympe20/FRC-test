9627a15b108ae124efdac6c7d822e245acfc331f
Kayla <kayla.blosser@proton.me>
2024-10-21 09:55:09 -0400

    Move intake roller to a new class

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -10,33 +10,41 @@ import frc.robot.commands.IntakeWrist;
 import com.revrobotics.CANSparkMax;
 
 public class Intake extends SubsystemBase {
-
   private double wristSetpoint = 0;
-  private double rollerSetpoint = 0;
   private DutyCycleEncoder encoder = new DutyCycleEncoder(0);
   private CANSparkMax wrist = new CANSparkMax(0, CANSparkMax.MotorType.kBrushless);
 
   public void setWristSetpoint(double setpoint) {
     this.wristSetpoint = setpoint;
   }
-  public double getWristSetpoint() {
+  private double getWristSetpoint() {
     return wristSetpoint;
-  }
-
-  public void setRollerSetpoint(double setpoint) {
-    this.rollerSetpoint = setpoint;
-  }
-  public double getRollerSetpoint() {
-    return rollerSetpoint;
   }
 
   public Intake() {
     //initialize IntakeWrist Command
     new IntakeWrist(encoder::getDistance, this::getWristSetpoint, wrist::set).schedule();
+    new IntakeRoller(1);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+}
+class IntakeRoller {
+  private CANSparkMax roller;
+
+  public IntakeRoller(int motorPort) {
+    //initialize IntakeRoller
+    roller = new CANSparkMax(motorPort, CANSparkMax.MotorType.kBrushless);
+  }
+
+  public void engageRoller() {
+    roller.set(1);
+  }
+
+  public void disengageRoller() {
+    roller.set(0);
   }
 }
